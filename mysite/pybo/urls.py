930a3a93657624286ad85a00d88bc410ee0a33af
Heewon-k <heewon.ms.kim@gmail.com>
2023-10-12 09:16:10 +0900

    10Oct class

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -6,5 +6,6 @@ app_name = 'pybo'  # 서로 다른 앱이 같은 url매핑 별칭을 쓸 때 중
 urlpatterns = [
     path('', views.index, name = 'index'),  # name 속성으로 url 매핑에 별칭만들기
     path('<int:question_id>/', views.detail, name = 'detail'),  # /pybo/2/ 요청시, question_id에 2가 저장 --> views.detail함수 실행
-    # path('answer/create/<int:question_id>', views.answer_create, name = 'answer_create'),
+    path('answer/create/<int:question_id>', views.answer_create, name = 'answer_create'),
+    path('question/create/', views.question_create, name = 'question_create'),
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from .models import Question
+from .forms import QuestionForm
 
 # Create your views here.
 def index(request):
@@ -21,8 +22,21 @@ def detail(request, question_id):
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context)
 
-# def answer_create(request, question_id):
-#     question = get_object_or_404(Question, pk = question_id)
-#     question.answer_set.create(content = request.POST.get('content'),
-#                                create_date = timezone.now())
-#     return redirect('pybo:detail', question_id = question.id)
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    question.answer_set.create(content = request.POST.get('content'),
+                               create_date = timezone.now())
+    return redirect('pybo:detail', question_id = question.id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm() # request.method가 'GET'인 경우 호출
+    context = {'form' : form}
+    return render(request, 'pybo/question_form.html', context)
